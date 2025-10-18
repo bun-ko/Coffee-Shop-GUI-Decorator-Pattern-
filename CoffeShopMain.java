69ae4e21afbdd21e1d5a7a07720629ebe147eeee
randomice1219 <andersoncc2@appstate.edu>
2025-10-17 23:56:47 -0400

    WE GOT PROBLEMS

---
+++ CoffeShopMain.java
@@ -0,0 +1,6 @@
+public class CoffeShopMain {
+    public static void main(String[] args){
+        Espresso espressoDemo = new Espresso();
+        Milk addMilk = new Milk(espressoDemo); // BUT ESPRESSO IS A TYPE OF BEVERAGE YOU FUCK
+    }
+}
--- Milk.java
+++ Milk.java
@@ -1,6 +1,11 @@
 public class Milk extends CondimentDecorator{
     Beverage beverage;
 
+    public Milk(Beverage beverage){ //IS THIS NOT A CONSTRUCTOR>???? JAVA WHAT
+        System.out.println(getCost());
+        System.out.println(getDescription());
+    }
+
     public double getCost(){
         return this.beverage.getCost() + .99; //whatever price this is
     }
